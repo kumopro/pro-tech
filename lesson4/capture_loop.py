5f86916f64e15c9b220f166badf90f26486054ad
ayaka nakatani <jellyfish167@hotmail.co.jp>
2018-05-05 11:51:43 +0900

    add new file for lesson4

---
+++ lesson4/capture_loop.py
@@ -0,0 +1,20 @@
+import picamera
+import readchar
+
+def main():
+    cam = picamera.PiCamera()
+    cam.resolution = (640, 480)
+    cam.start_preview()
+
+    while True:
+        key = readchar.readkey()
+
+        if key == "q":
+            break
+        elif key == "c":
+            for i in range(3):
+                fname = "my_picture_{}.png".format(i)
+                cam.capture('my_picture.png')
+
+    cam.stop_preview()
+main()
