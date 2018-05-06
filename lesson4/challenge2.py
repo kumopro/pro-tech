99b97b01fff832f510cf78fb1cfd779dc1eaa54b
ayaka nakatani <jellyfish167@hotmail.co.jp>
2018-05-06 16:26:38 +0900

    add new files for lesson4

---
+++ lesson4/challenge2.py
@@ -0,0 +1,9 @@
+for i in range(1, 11):
+    if i == 1:
+        print("1st prize")
+    elif i == 2:
+        print("2nd prize")
+    elif i == 3:
+        print("3rd prize")
+    else:
+        print("{}th prize".format(i))
