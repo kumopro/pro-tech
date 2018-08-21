eaa381b13c202d1222a36c0b75086770893e83f0
ayaka nakatani <jellyfish167@hotmail.co.jp>
2018-08-21 17:29:59 +0900

    fix bug

--- lesson8/get_weather_led.py
+++ lesson8/get_weather_led.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import wiringpi
+import time
 
 def get_tomorrow_weather():
     url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
@@ -8,8 +9,9 @@ def get_tomorrow_weather():
 
     forecasts = data['forecasts']
     tomorrow_forecast = forecasts[1]
-    tomorrow_weather = forecast_tomorrow['telop']
+    tomorrow_weather = tomorrow_forecast['telop']
     print(tomorrow_weather)
+
     return tomorrow_weather
 
 def main():    
@@ -17,13 +19,14 @@ def main():
     wiringpi.wiringPiSetupGpio()
     wiringpi.pinMode(led_pin, wiringpi.OUTPUT)
     
-    tomorrow_weather = get_tomorrow_weather
+    tomorrow_weather = get_tomorrow_weather()
 
     if '雨' in tomorrow_weather:
         print('明日は雨が降ります')
         wiringpi.digitalWrite(led_pin, wiringpi.HIGH)
+        time.sleep(5)
+        wiringpi.digitalWrite(led_pin, wiringpi.LOW)
     else:
         print('明日は雨が降りません')
-        wiringpi.digitalWrite(led_pin, wiringpi.LOW)
 
 main()
