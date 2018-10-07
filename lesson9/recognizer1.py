559b7c1d08e1765bcd3aa84ccbce94151f68b59e
ayaka nakatani <jellyfish167@hotmail.co.jp>
2018-10-07 19:35:21 +0900

    update file

--- lesson9/recognizer1.py
+++ lesson9/recognizer1.py
@@ -1,13 +1,20 @@
 from watson_developer_cloud import VisualRecognitionV3
+import json
 
 def recognize():
-    version = '2016-05-20'
-    api_key = ''
-    filename = ''
+    version = '2018-03-19'
+    api_key = 'outJYFDh3fDNNwJcqQIzb09rDNAqZX-5iwJvilfENioc'
+    filename = 'dog.jpg'
 
     recognizer = VisualRecognitionV3(version=version, iam_apikey=api_key)
     with open (filename, 'rb') as image:
-        results = recognizer.classify(image)
+        results = recognizer.classify(image).get_result()
+    print(json.dumps(results, indent=2))
+    print(type(results))
     print(results)
+    r = results['images'][0]['classifiers'][0]['classes']
+
+    for i in range(3):
+        print(r[i])
 
 recognize()
