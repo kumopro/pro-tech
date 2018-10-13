a8836a695e5e0da412bd37373adf79a6bf5c4a26
aya <jellyfish@gmail.com>
2018-10-13 16:12:10 +0900

    update file

--- lesson9/face_detector.py
+++ lesson9/face_detector.py
@@ -10,10 +10,15 @@ def recognize():
     with open (filename, 'rb') as image:
         results = recognizer.detect_faces(image).get_result()
 
-    print(json.dumps(results))
+    # print(json.dumps(results))
     r = results['images'][0]['faces']
 
-    for i in range(1):
-        print(r[i])
+    age_min = float(r[0]['age']['min'])
+    age_max = float(r[0]['age']['max'])
+    age = int((age_max + age_min) / 2.0 + 0.5)
+    print("age: {}".format(age))
+
+    gender = r[0]['gender']['gender']
+    print("gender: {}".format(gender))
 
 recognize()
